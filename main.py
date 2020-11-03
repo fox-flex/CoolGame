5bfe9f818db56bc54ceec1228d1bd93a0cf34f18
Fox <you@example.com>
2020-11-03 16:52:48 +0200

    add animation meteor

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ pygame.init()
 win = pygame.display.set_mode((1000, 720))
 clock = pygame.time.Clock()
 
-x = 0
 y = -80
 width = 40
 height = 60
@@ -16,10 +15,17 @@ x = randint(100, 400)
 present_state = 0
 
 bg = pygame.image.load("images/bg.jpg")
+meteor = []
+for i in range(1, 11):
+    meteor.append(pygame.image.load("images/meteor/meteor" + str(i) + ".jpg"))
+meteor_num = 0
 
 
 def drawWindow():
+    global meteor_num
     win.blit(bg, (0, 0))
+    win.blit(meteor[meteor_num], (x, y))
+    meteor_num = (meteor_num + 1) % 10
     pygame.display.update()
 
 
@@ -41,6 +47,5 @@ while run:
         x = randint(100, 400)
 
     drawWindow()
-    pygame.draw.circle(win, (255, 149, 0), (x, y), (radius))
     pygame.display.update()
 pygame.quit()
